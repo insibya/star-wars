8870d8d8809bf1ee632ec73b507834f6432cc398
Nathan Roy <nathanmroy@yahoo.com>
2020-01-11 09:14:47 -0800

    added result search fields but no functionality

--- app.js
+++ app.js
@@ -16,14 +16,20 @@ const loadData = async () => {
 	renderData(starships, 'starships');
 };
 
-const renderData = (dataList, display) => {
-	console.log(dataList.results);
+const renderData = (dataList, id) => {
 	HTML = dataList.results
 		.map((datum) => {
 			return `<li>${datum.title ? datum.title : datum.name}</li>`;
 		})
 		.join('');
-	document.querySelector(`#${display}`).innerHTML = `<ul>${display}: ${HTML}</ul>`;
+	document.querySelector(`#${id}`).innerHTML = `<input type='text' id='${id}Form'><ul>${id[0].toUpperCase() +
+		id.slice(1)}: ${HTML}</ul>`;
+
+	console.log(sortByNameAsTyped(dataList, id, 'The'));
+};
+
+const sortByNameAsTyped = (dataList, id, searchValue) => {
+	return dataList;
 };
 
 loadData();
